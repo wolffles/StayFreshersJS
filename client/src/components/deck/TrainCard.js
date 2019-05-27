8ff0ec413cae4234c2de66db6fbdcf66238796d4
woffles <wolfgang.truong@gmail.com>
2019-05-27 12:00:07 -0700

    working: click to flip feature

--- client/src/components/deck/TrainCard.js
+++ client/src/components/deck/TrainCard.js
@@ -6,24 +6,23 @@ class TrainCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      TorD:"term"
+     termDef: false,
+     face: ""
     }
     this.onClick= this.onClick.bind(this);
   }
 
   onClick(e){
-    this.state.TorD === "term" ? this.setState({ TorD: "definition" }) : this.setState({ TorD: "term" })
+    let face; 
+    this.state.termDef ? face = '' : face = 'face'
+    this.setState({termDef: !this.state.termDef, face: face})
   }
+
+
   
   render() {
-    // let fresher;
-    // if (this.state.TorD === "term") {
-    //   fresher = <div className={`flashcard term ${this.state.overflow_term}`} onClick={this.onClick}><p>{this.props.card.term}</p></div>
-    // } else {
-    //   fresher = <div className={`flashcard definition ${this.state.overflow_definition}`} onClick={this.onClick}><p>{this.props.card.definition}</p></div>
-    // }
     return (
-      <div className="freshers m-2">
+      <div className={"freshers m-2 " + this.state.face} onClick={this.onClick}>
         <div className={`term ${this.props.term}`} >{this.props.card.term}</div>
         <div className={`${this.props.definition} definition`} >{this.props.card.definition}</div>
         
